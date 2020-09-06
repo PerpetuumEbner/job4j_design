0c02a97b69df1827b03a8e4ac859054bfd2d90f1
PerpetuumEbner <yustas@protonmail.com>
2020-09-06 14:04:09 +0500

    1.3.5. Очередь на двух стеках. 2.

--- chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -5,17 +5,24 @@ public class SimpleQueue<T> {
     private final SimpleStack<T> out = new SimpleStack<>();
     private int size;
 
-    public T poll() {
+    public T poll() { // достать элемент
         T value = in.pop();
-        while (size != 0) {
-            value = in.pop();
-            out.push(value);
+        if (isEmpty()) {
+            while (!isEmpty()) {
+                value = in.pop();
+                out.push(value);
+                size--;
+            }
         }
         return value;
     }
 
-    public void push(T value) {
+    public void push(T value) { // добавляет
         in.push(value);
         size++;
+    }
+
+    public boolean isEmpty() {
+        return (size == 0);
     }
 }
