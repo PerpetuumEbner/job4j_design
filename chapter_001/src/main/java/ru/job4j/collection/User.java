efd18b17e0f3319d57768094ddb6d005d9ad19f7
PerpetuumEbner <yustas@protonmail.com>
2020-09-15 13:12:10 +0500

    1.5.2. Без переопределения equals и hashCode.

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -12,4 +12,16 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
+
+    public static void main(String[] args) {
+        User user1 = new User("Sasha", 1, new GregorianCalendar(2020, 1, 1));
+        User user2 = new User("Sasha", 1, new GregorianCalendar(2020, 1, 1));
+        Map<User, Object> userMap = new HashMap<>();
+        userMap.put(user1, "first");
+        userMap.put(user2, "second");
+
+        for (Map.Entry<User, Object> entry : userMap.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
 }
