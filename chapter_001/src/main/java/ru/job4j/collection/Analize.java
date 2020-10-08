0198c8b49e5681ee55105bbce4ed87e000360447
PerpetuumEbner <yustas@protonmail.com>
2020-10-08 22:28:30 +0500

    1.7.2. Статистика по коллекции. 2.

--- chapter_001/src/main/java/ru/job4j/collection/Analize.java
+++ chapter_001/src/main/java/ru/job4j/collection/Analize.java
@@ -1,30 +1,85 @@
 package ru.job4j.collection;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Analize {
     public Info diff(List<User> previous, List<User> current) {
+        Info info = new Info();
+
         int added = 0;
         int changed = 0;
         int deleted = 0;
 
-        for (int index = 0; index < previous.size(); index++) {
-            if (!previous.get(index).name.equals(current.get(index).name) && current.get(index).name != null) {
+        Map<Integer, User> mapCurrent = current.stream().collect(Collectors.toMap(User::getId, user -> user));
+        for (int i = 0; i < previous.size(); i++) {
+            User user = previous.get(i);
+            if (!mapCurrent.containsKey(user.getId())) {
+                added++;
+            }
+
+            if (!mapCurrent.get(user.getId()).getName().equals(user.getName())
+                    && mapCurrent.get(user.getId()).getName() != null) {
                 changed++;
             }
-            if (!previous.get(index).name.isEmpty() && previous.get(index).name == null) {
-                deleted++;
-            }
-            if (previous.get(index).name.isEmpty() && previous.get(index).name != null) {
+
+            if (user.getName() != null
+                    && mapCurrent.get(user.getId()).getName() == null) {
                 deleted++;
             }
         }
-        return null;
+
+        info.added = added;
+        info.changed = changed;
+        info.deleted = deleted;
+
+        return info;
     }
 
     public static class User {
         int id;
         String name;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            User user = (User) o;
+            return id == user.id
+                    && Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
     }
 
     public static class Info {
---
+++ chapter_001/src/test/java/ru/job4j/collection/AnalizeTest.java
@@ -0,0 +1,67 @@
+package ru.job4j.collection;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AnalizeTest {
+    @Test
+    public void whenAdded() {
+        Analize userAnalize = new Analize();
+
+        List<Analize.User> usersPrevious = Arrays.asList(
+                new Analize.User(1, "Mike"),
+                new Analize.User(2, "Jon"),
+                new Analize.User(3, "Bob"));
+
+        List<Analize.User> usersCurrent = Arrays.asList(
+                new Analize.User(1, "Mike"),
+                new Analize.User(2, "Jon"),
+                new Analize.User(3, "Bob"),
+                new Analize.User(4, "Mia"),
+                new Analize.User(5, "Alexa"));
+
+        int added = userAnalize.diff(usersPrevious, usersCurrent).added;
+        assertThat(added, is(2));
+    }
+
+    @Test
+    public void whenChanged() {
+        Analize userAnalize = new Analize();
+
+        List<Analize.User> usersPrevious = Arrays.asList(
+                new Analize.User(1, "Mike"),
+                new Analize.User(2, "Jon"),
+                new Analize.User(3, "Bob"));
+
+        List<Analize.User> usersCurrent = Arrays.asList(
+                new Analize.User(1, "Mike"),
+                new Analize.User(2, "Mia"),
+                new Analize.User(3, "Bob"));
+
+        int changed = userAnalize.diff(usersPrevious, usersCurrent).changed;
+        assertThat(changed, is(1));
+    }
+
+    @Test
+    public void whenDeleted() {
+        Analize userAnalize = new Analize();
+
+        List<Analize.User> usersPrevious = Arrays.asList(
+                new Analize.User(1, "Mike"),
+                new Analize.User(2, "Jon"),
+                new Analize.User(3, "Bob"));
+
+        List<Analize.User> usersCurrent = Arrays.asList(
+                new Analize.User(1, ""),
+                new Analize.User(2, "Jon"),
+                new Analize.User(3, "Bob"));
+
+        int deleted = userAnalize.diff(usersPrevious, usersCurrent).deleted;
+        assertThat(deleted, is(1));
+    }
+}
