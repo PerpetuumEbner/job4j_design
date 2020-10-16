2e526238eb4d6f5c3124c5f4928d35f206c2d858
PerpetuumEbner <yustas@protonmail.com>
2020-10-16 15:47:56 +0500

    2.1.0.2 FileInputStream. 2.

--- chapter_001/src/main/java/ru/job4j/io/EvenNumberFile.java
+++ chapter_001/src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -4,18 +4,35 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class EvenNumberFile {
+    public static void checkNumber(Integer i) {
+        if (i % 2 == 0) {
+            System.out.println(i + " чётное");
+        } else {
+            System.out.println(i + " нечётное ");
+        }
+    }
+
     public static void main(String[] args) {
+        int i;
         try (FileInputStream fis = new FileInputStream("even.txt")) {
+            StringBuilder sb = new StringBuilder();
             int read;
             while ((read = fis.read()) != -1) {
-                if (read % 2 == 0) {
-                    System.out.println(read + " чётное");
-                } else {
-                    System.out.println(read + " нечётное ");
+                if (read == 10) {
+                    i = Integer.parseInt(sb.toString());
+                    checkNumber(i);
+                    sb.delete(0, sb.length());
+                }
+                if (read < 58 && read > 47) {
+                    sb.append((char) read);
                 }
             }
+            i = Integer.parseInt(sb.toString());
+            checkNumber(i);
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.out.println("Не верный формат");
         }
     }
 }
