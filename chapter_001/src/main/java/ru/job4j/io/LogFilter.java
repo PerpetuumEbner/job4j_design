cc8197aa2890a8f9882180ac58894798c964ea2e
PerpetuumEbner <yustas@protonmail.com>
2020-10-16 22:46:33 +0500

    2.1.0.4. BufferedOutputStream.

--- chapter_001/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_001/src/main/java/ru/job4j/io/LogFilter.java
@@ -1,8 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,8 +21,17 @@ public class LogFilter {
         return list;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter pw = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+                pw.write(String.valueOf(log));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
         System.out.println(log);
+        save(log, "404.txt");
     }
 }
