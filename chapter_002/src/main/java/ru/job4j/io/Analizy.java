eff68866ffa59bb9ba3b62aaf1f2de622af17d73
PerpetuumEbner <yustas@protonmail.com>
2020-10-22 23:38:09 +0500

    2.1.3. Тестирование IO.

--- chapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -5,14 +5,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Analizy {
-    private final String pathSource;
-    private final String pathTarget;
     private final Map<String, String> values = new LinkedHashMap<>();
-
-    public Analizy(String source, String target) {
-        this.pathSource = source;
-        this.pathTarget = target;
-    }
 
     public void load(String source) {
         String startTime;
@@ -44,7 +37,7 @@ public class Analizy {
     public void safe(String target) {
         try (PrintWriter pw = new PrintWriter(target)) {
             for (Map.Entry<String, String> entry : values.entrySet()) {
-                pw.write(entry.getKey() + ";" + entry.getValue() + "\n");
+                pw.write(entry.getKey() + ";" + entry.getValue() + System.lineSeparator());
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -54,14 +47,5 @@ public class Analizy {
     public void unavailable(String source, String target) {
         load(source);
         safe(target);
-    }
-
-    public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,13 +1,36 @@
 package ru.job4j.io;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
-    public void whenAnalizyLog() {
-        String pathSource = "./src/main/resources/source.csv";
-        String pathTarget = "./src/main/resources/target.csv";
-        Analizy analizy = new Analizy(pathSource, pathTarget);
-        analizy.unavailable(pathSource, pathTarget);
+    public void whenAnalizyLog() throws IOException {
+        File source = folder.newFile("source.csv");
+        File target = folder.newFile("target.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01" + System.lineSeparator()
+                    + "200 10:57:01" + System.lineSeparator()
+                    + "400 10:58:01" + System.lineSeparator()
+                    + "200 10:59:01" + System.lineSeparator()
+                    + "500 11:01:02" + System.lineSeparator()
+                    + "200 11:02:02");
+        }
+        new Analizy().unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(target))) {
+            br.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:58:01;10:59:01"
+                + "11:01:02;11:02:02"));
     }
 }
