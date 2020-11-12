45a7189d791952499237694c4c55084d1fccd6b7
PerpetuumEbner <yustas@protonmail.com>
2020-11-13 00:18:18 +0500

    2.3.5. Преобразование JSON в POJO. JsonObject.

--- chapter_002/src/main/java/ru/job4j/serialization/CarBrandName.java
+++ chapter_002/src/main/java/ru/job4j/serialization/CarBrandName.java
@@ -2,6 +2,15 @@ package ru.job4j.serialization;
 
 public class CarBrandName {
     private final String brandName;
+    private Cars cars;
+
+    public Cars getCars() {
+        return cars;
+    }
+
+    public void setCars(Cars cars) {
+        this.cars = cars;
+    }
 
     public CarBrandName(String brandName) {
         this.brandName = brandName;
--- chapter_002/src/main/java/ru/job4j/serialization/Cars.java
+++ chapter_002/src/main/java/ru/job4j/serialization/Cars.java
@@ -2,14 +2,23 @@ package ru.job4j.serialization;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
 public class Cars {
-    private final CarBrandName brandName;
+    private CarBrandName brandName;
     private final int yearOfIssue;
     private final boolean condition;
     private final String[] completion;
+
+    public CarBrandName getBrandName() {
+        return brandName;
+    }
+
+    public void setCarBrandName(CarBrandName brandName) {
+        this.brandName = brandName;
+    }
 
     public Cars(CarBrandName brandName, int yearOfIssue, boolean condition, String... completion) {
         this.brandName = brandName;
@@ -30,6 +39,7 @@ public class Cars {
 
     public static void main(String[] args) {
         final Cars cars = new Cars(new CarBrandName("ŠKODA"), 2019, true, "Heated steering wheel", "Cruise control", "ABS");
+        CarBrandName carBrandName = new CarBrandName("ŠKODA");
 
         final Gson gson = new GsonBuilder().create();
         System.out.println(gson.toJson(cars));
@@ -40,5 +50,9 @@ public class Cars {
                 + "\"completion\": [\"Heated steering wheel\", \"Cruise control\", \"ABS\"]}";
         final Cars carsMode = gson.fromJson(carsJson, Cars.class);
         System.out.println(carsMode);
+
+        cars.setCarBrandName(carBrandName);
+        carBrandName.setCars(cars);
+        System.out.println(new JSONObject(carBrandName));
     }
 }
