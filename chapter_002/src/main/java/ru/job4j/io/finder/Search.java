87573fcc7f73e153d22d630f55ccd7e1fe3f6f20
PerpetuumEbner <yustas@protonmail.com>
2020-11-16 18:58:37 +0500

    2.5. Поиск файлов по критерию.

---
+++ chapter_002/src/main/java/ru/job4j/io/finder/Search.java
@@ -0,0 +1,31 @@
+package ru.job4j.io.finder;
+
+import ru.job4j.io.SearchFiles;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Invalid number of arguments.");
+        }
+
+        Path start = Paths.get(args[0]);
+        String extension = args[1];
+        search(start, extension).forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}
