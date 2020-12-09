76388a8251f58bc11fddd82efa851809d3db2c68
PerpetuumEbner <yustas@protonmail.com>
2020-12-09 22:28:35 +0500

    3.1.1. Схема прав пользователи и роли. 2.

--- chapter_003/src/main/java/scheme/systemelements.sql
+++ chapter_003/src/main/java/scheme/systemelements.sql
@@ -1,13 +1,12 @@
 create table users (
-    id serial primary key,
+	id serial primary key,
     name varchar(2000),
-	request_id int references request(id)
+	role_id int references role(id)
 );
 
 create table roles (
     id serial primary key,
     name varchar(2000),
-	users_id int references users(id)
 );
 
 create table rules (
@@ -17,36 +16,36 @@ create table rules (
 
 create table roles_rules (
     id serial primary key,
-    roles_id int references roles(id),
-	rules_id int references rules(id),
-	attached_files int references attached_files(id)
+	role_id int references role(id)
+	rules_id int references rules(id)
 );
 
 create table request (
     id serial primary key,
     name varchar(2000),
-	comments_id int references comments(id),
-	comments_id int references attached_files(id)
+	users_id int references users(id),
+	category_id int references category(id)
+	state_id int references state(id)
 );
 
 create table comments (
     id serial primary key,
     name varchar(2000)
+	request_id int references request(id)
 );
 
 create table attached_files (
     id serial primary key,
     name varchar(2000)
+	request_id int references request(id)
 );
 
 create table category (
     id serial primary key,
     name varchar(2000),
-	request_id int references request(id)
 );
 
 create table state (
     id serial primary key,
     name varchar(2000),
-	request_id int references request(id)
 );
