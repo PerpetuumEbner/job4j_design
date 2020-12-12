bad94ceaae46dd58b43c8a071c98d2644b175799
PerpetuumEbner <yustas@protonmail.com>
2020-12-12 22:55:02 +0500

    3.1.1. Схема прав пользователи и роли. 3.

--- chapter_003/src/main/java/scheme/systemelements.sql
+++ chapter_003/src/main/java/scheme/systemelements.sql
@@ -1,51 +1,60 @@
-create table users (
-	id serial primary key,
-    name varchar(2000),
-	role_id int references role(id)
+create table users
+(
+    id      serial primary key,
+    name    varchar(2000),
+    role_id int references roles (id)
 );
 
-create table roles (
-    id serial primary key,
-    name varchar(2000),
-);
-
-create table rules (
-    id serial primary key,
+create table roles
+(
+    id   serial primary key,
     name varchar(2000)
 );
 
-create table roles_rules (
-    id serial primary key,
-	role_id int references role(id)
-	rules_id int references rules(id)
-);
-
-create table request (
-    id serial primary key,
-    name varchar(2000),
-	users_id int references users(id),
-	category_id int references category(id)
-	state_id int references state(id)
-);
-
-create table comments (
-    id serial primary key,
+create table rules
+(
+    id   serial primary key,
     name varchar(2000)
-	request_id int references request(id)
 );
 
-create table attached_files (
-    id serial primary key,
+create table roles_rules
+(
+    id       serial primary key,
+    role_id  int references roles (id),
+    rules_id int references rules (id)
+);
+
+create table category
+(
+    id   serial primary key,
     name varchar(2000)
-	request_id int references request(id)
 );
 
-create table category (
-    id serial primary key,
-    name varchar(2000),
+create table state
+(
+    id   serial primary key,
+    name varchar(2000)
 );
 
-create table state (
-    id serial primary key,
-    name varchar(2000),
+create table item
+(
+    id          serial primary key,
+    name        varchar(2000),
+    users_id    int references users (id),
+    category_id int references category (id),
+    state_id    int references state (id)
+);
+
+create table comments
+(
+    id         serial primary key,
+    name       varchar(2000),
+    request_id int references item (id)
+);
+
+create table attached_files
+(
+    id         serial primary key,
+    name       varchar(2000),
+    request_id int references item (id)
 );
