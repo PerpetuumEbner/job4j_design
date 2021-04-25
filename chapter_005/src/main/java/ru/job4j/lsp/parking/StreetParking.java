a3678a9da6a66bd0155c4811ce6d7edbcbc9a7dc
PerpetuumEbner <yustas@protonmail.com>
2021-04-25 16:22:24 +0500

    5.3.2. Парковка машин. Реализация тестов. 2.

--- chapter_005/src/main/java/ru/job4j/lsp/parking/StreetParking.java
+++ chapter_005/src/main/java/ru/job4j/lsp/parking/StreetParking.java
@@ -4,6 +4,13 @@ public class StreetParking implements Parking {
     private int[][] carPlace;
     private int[][] truckPlace;
     private int carSize = 1;
+    private int placesForTrucks;
+    private int placesForCars;
+
+    public StreetParking(int placesForTrucks, int placesForCars) {
+        this.placesForTrucks = placesForTrucks;
+        this.placesForCars = placesForCars;
+    }
 
     @Override
     public boolean park(Car car) {
--- chapter_005/src/test/java/ru/job4j/lsp/parking/StreetParkingTest.java
+++ chapter_005/src/test/java/ru/job4j/lsp/parking/StreetParkingTest.java
@@ -4,21 +4,58 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class StreetParkingTest {
 
     @Ignore
     @Test
     public void whenPickUp() {
-        Parking parking = new StreetParking();
+        Parking parking = new StreetParking(2, 2);
         Car car = new Car("BMW", 1);
         Assert.assertTrue(parking.park(car));
     }
 
     @Ignore
+    @Test
+    public void whenUnsuccessfulPickUp() {
+        Parking parking = new StreetParking(2, 2);
+        Car car = new Car("BMW", 1);
+        assertThat(parking.park(car), is(0));
+    }
+
+    @Ignore
     @Test(expected = IllegalArgumentException.class)
     public void whenTheSizeIsWrong() {
-        Parking parking = new StreetParking();
+        Parking parking = new StreetParking(2, 2);
         Car car = new PassengerCar("VW");
         Assert.assertTrue(parking.park(car));
+    }
+
+    @Ignore
+    @Test
+    public void whenPickUpTruck() {
+        Parking parking = new StreetParking(2, 2);
+        Car car = new Truck("MAN", 3);
+        assertThat(parking.park(car), is(1));
+    }
+
+    @Ignore
+    @Test
+    public void whenUnsuccessfulPickUpTruck() {
+        Parking parking = new StreetParking(1, 3);
+        parking.park(new Truck("Mercedes", 4));
+        Car car = new Truck("Mercedes", 3);
+        assertThat(parking.park(car), is(0));
+    }
+
+    @Ignore
+    @Test
+    public void whenTruckParkedSeveralPlacesOfPassengerCars() {
+        Parking parking = new StreetParking(1, 4);
+        parking.park(new Truck("Volvo", 4));
+        Car car = new Truck("Renault", 4);
+        assertThat(parking.park(car), is(1));
     }
 }
