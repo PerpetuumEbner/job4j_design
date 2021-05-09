1c2ec7683148028eba592d4117a5c1cdff9993d8
PerpetuumEbner <yustas@protonmail.com>
2021-05-09 22:10:08 +0500

    5.3.2. Парковка машин. Реализация кода. 2.

--- chapter_005/src/main/java/ru/job4j/lsp/parking/StreetParking.java
+++ chapter_005/src/main/java/ru/job4j/lsp/parking/StreetParking.java
@@ -1,12 +1,13 @@
 package ru.job4j.lsp.parking;
 
+import java.util.stream.IntStream;
+
 public class StreetParking implements Parking {
     private final Car[] carPlaces;
     private final Car[] truckPlaces;
     private final int carSize = 1;
     private int placesForCars;
     private int placesForTrucks;
-
 
     public StreetParking(int carPlaces, int truckPlaces) {
         this.carPlaces = new Car[carPlaces];
@@ -16,21 +17,34 @@ public class StreetParking implements Parking {
     @Override
     public boolean park(Car car) {
         boolean result = true;
-        if (car.getSize() < carSize) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
-        if (placesForCars < carPlaces.length && car.getSize() == carSize) {
-            carPlaces[placesForCars++] = car;
-        }
-        if (placesForTrucks < truckPlaces.length) {
-            truckPlaces[placesForTrucks++] = car;
-        } else if (placesForCars < carPlaces.length && car.getSize() > carSize) {
-            int size = car.getSize();
-            for (int index = 1; index < size; index++) {
+        if (checkSizeMachine(car)) {
+            if (placesForCars < carPlaces.length) {
                 carPlaces[placesForCars++] = car;
+            } else {
+                result = false;
             }
         } else {
+            if (placesForTrucks < truckPlaces.length) {
+                truckPlaces[placesForTrucks++] = car;
+            } else if (placesForTrucks > truckPlaces.length || placesForCars >= carPlaces.length) {
+                result = false;
+            } else {
+                IntStream.range(0, car.getSize()).forEachOrdered(index -> carPlaces[placesForCars++] = car);
+            }
+        }
+        return result;
+    }
+
+    public boolean checkSizeMachine(Car car) {
+        boolean result = false;
+        if (car.getSize() == carSize) {
+            result = true;
+        }
+        if (car.getSize() > carSize) {
             result = false;
+        }
+        if (car.getSize() < carSize) {
+            throw new ArrayIndexOutOfBoundsException("Размер машины не может быть меньше единицы.");
         }
         return result;
     }
--- chapter_005/src/test/java/ru/job4j/lsp/parking/StreetParkingTest.java
+++ chapter_005/src/test/java/ru/job4j/lsp/parking/StreetParkingTest.java
@@ -3,9 +3,6 @@ package ru.job4j.lsp.parking;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 public class StreetParkingTest {
 
     @Test
@@ -18,39 +15,40 @@ public class StreetParkingTest {
     @Test
     public void whenUnsuccessfulPark() {
         Parking parking = new StreetParking(1, 2);
-        Car car = new Car("BMW", 1);
-        parking.park(car);
-        Assert.assertFalse(parking.park(new Car("VW", 1)));
+        parking.park(new Car("BMW", 1));
+        Car car = new PassengerCar("VW", 1);
+        Assert.assertFalse(parking.park(car));
     }
 
     @Test(expected = ArrayIndexOutOfBoundsException.class)
     public void whenTheSizeIsWrong() {
         Parking parking = new StreetParking(2, 2);
-        Car car = new Car("VW", 0);
+        Car car = new PassengerCar("VW", 0);
         Assert.assertTrue(parking.park(car));
     }
 
     @Test
     public void whenParkTruck() {
         Parking parking = new StreetParking(2, 2);
-        Car car = new Truck("MAN", 3);
+        parking.park(new Truck("MAN", 3));
+        Car car = new Truck("Volvo", 3);
         Assert.assertTrue(parking.park(car));
     }
 
     @Test
     public void whenUnsuccessfulParkTruck() {
-        Parking parking = new StreetParking(1, 3);
+        Parking parking = new StreetParking(4, 1);
         parking.park(new Truck("Mercedes", 4));
-        Car car = new Truck("Mercedes", 3);
-        Assert.assertTrue(parking.park(car));
-        assertThat(parking.park(car), is(0));
+        parking.park(new Truck("Renault", 4));
+        Car car = new Truck("MAN", 3);
+        Assert.assertFalse(parking.park(car));
     }
 
     @Test
     public void whenTruckParkedSeveralPlacesOfPassengerCars() {
-        Parking parking = new StreetParking(1, 4);
-        parking.park(new Truck("Volvo", 4));
-        Car car = new Truck("Renault", 4);
-        assertThat(parking.park(car), is(1));
+        Parking parking = new StreetParking(3, 1);
+        parking.park(new Truck("Volvo", 2));
+        Car car = new Truck("Renault", 2);
+        Assert.assertTrue(parking.park(car));
     }
 }
