ac8675df2144cb5bcaf4bf0a459e7566030b070a
Stanislav <yustas@protonmail.com>
2021-12-03 23:59:50 +0500

    2.1.7. Scanner. 2.

--- chapter_002/src/main/java/ru/job4j/io/CSVReader.java
+++ chapter_002/src/main/java/ru/job4j/io/CSVReader.java
@@ -8,13 +8,11 @@ import java.io.File;
 
 public class CSVReader {
     private static String[] args;
+    private static ArgsName argsName;
 
-    public String[] getArgs() {
-        return args;
-    }
+    private static boolean rsl = true;
 
     public static boolean validate() throws IllegalAccessException {
-        boolean rsl = true;
         path();
         delimiter();
         receiver();
@@ -26,46 +24,46 @@ public class CSVReader {
     }
 
     public static String path() throws IllegalAccessException {
-        File path = new File(args[0].substring(3));
+        File path = new File(argsName.get("path"));
         if (!path.isDirectory()) {
             throw new IllegalAccessException("Invalid directory.");
         }
-        return args[0].substring(3);
+        return argsName.get("path");
     }
 
     public static String delimiter() throws IllegalAccessException {
-        Pattern pattern = Pattern.compile("\\s\\S");
-        if (!args[1].substring(11).equals(pattern)) {
+        Pattern pattern = Pattern.compile("\\W");
+        if (!argsName.get("delimiter").equals(pattern)) {
             throw new IllegalAccessException("Invalid delimiter");
         }
-        return args[1].substring(11);
+        return argsName.get("delimiter");
     }
 
-    public static String receiver() throws IllegalAccessException {
-        if (!args[2].substring(5).equals("stdout")) {
+    public static void receiver() throws IllegalAccessException {
+        if (!argsName.get("out").equals("stdout")) {
             throw new IllegalAccessException("Invalid parameter.");
         }
-        return args[2].substring(5);
     }
 
-    public static String filter() throws IllegalAccessException {
-        if (args[3].substring(8).isEmpty()) {
+    public static void filter() throws IllegalAccessException {
+        if (argsName.get("filter").isEmpty()) {
             throw new IllegalAccessException("Invalid parameter.");
         }
-        return args[3].substring(8);
+        argsName.get("filter");
     }
 
-    public static void handle(ArgsName argsName, String path, String delimiter) throws Exception {
+    public static void handle(ArgsName argsName) throws Exception {
         List<Integer> list = new ArrayList<>();
         if (validate()) {
-            try (Scanner scanner = new Scanner(path)) {
-                scanner.useDelimiter(delimiter);
+            try (Scanner scanner = new Scanner(argsName.get("path"))) {
+                scanner.useDelimiter(argsName.get("delimiter"));
                 String[] column = scanner.next().split(",");
                 String[] args = argsName.get("filter").split(",");
                 for (String arg : args) {
                     for (int index = 0; index < column.length; index++) {
                         if (arg.equals(column[index])) {
                             list.add(index);
+                            break;
                         }
                     }
                 }
