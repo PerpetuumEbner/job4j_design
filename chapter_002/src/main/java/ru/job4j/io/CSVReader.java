c9b52f456a4cb2a62ba08b502dd20a1b6de8a8a5
Stanislav <yustas@protonmail.com>
2021-12-20 16:46:12 +0500

    2.1.7. Scanner. 5.

--- chapter_002/src/main/java/ru/job4j/io/CSVReader.java
+++ chapter_002/src/main/java/ru/job4j/io/CSVReader.java
@@ -1,14 +1,12 @@
 package ru.job4j.io;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.io.File;
-
 public class CSVReader {
     private static ArgsName argsName;
     private static int size;
-
+    private static String[] column;
     public static boolean validate() throws IllegalAccessException {
         path();
         delimiter();
@@ -16,7 +14,6 @@ public class CSVReader {
         filter();
         return size == 4;
     }
-
     public static String path() throws IllegalAccessException {
         File file = new File(argsName.get("path"));
         if (!file.exists()) {
@@ -25,7 +22,6 @@ public class CSVReader {
         size++;
         return argsName.get("path");
     }
-
     public static void delimiter() throws IllegalAccessException {
         if (argsName.get("delimiter").equals(";" + System.lineSeparator())
                 || argsName.get("delimiter").equals("," + System.lineSeparator())) {
@@ -34,7 +30,6 @@ public class CSVReader {
         size++;
         argsName.get("delimiter");
     }
-
     public static void receiver() throws IllegalAccessException {
         File file = new File(argsName.get("out"));
         if (argsName.get("out").equals("stdout") || !file.isFile()) {
@@ -42,7 +37,6 @@ public class CSVReader {
         }
         size++;
     }
-
     public static void filter() throws IllegalAccessException {
         if (argsName.get("filter").isEmpty()) {
             throw new IllegalAccessException("Invalid parameter.");
@@ -50,27 +44,34 @@ public class CSVReader {
         size++;
         argsName.get("filter");
     }
-
     public static void handle(ArgsName argsName) throws Exception {
         List<Integer> list = new ArrayList<>();
         CSVReader.argsName = argsName;
         if (validate()) {
-            try (Scanner scanner = new Scanner(argsName.get("path"))) {
-                scanner.useDelimiter(argsName.get("delimiter"));
-                String[] column = scanner.next().split(",");
-                String[] args = argsName.get("filter").split(",");
-                for (String arg : args) {
-                    for (int index = 0; index < column.length; index++) {
-                        if (arg.equals(column[index])) {
-                            list.add(index);
-                            break;
+            try (Scanner scanner = new Scanner(new File(argsName.get("path")))) {
+                parse(list);
+                while (scanner.hasNext()) {
+                    column = scanner.next().split(argsName.get("delimiter"));
+                    for (int index = 0; index < list.size(); index++) {
+                        if (index == list.size() - 1) {
+                            System.out.print(column[index] + System.lineSeparator());
+                        } else {
+                            System.out.print(column[index] + ";");
                         }
                     }
                 }
-                while (scanner.hasNext()) {
-                    column = scanner.next().split(",");
-                    for (Integer integer : list) {
-                        System.out.println(column[integer]);
+            }
+        }
+    }
+    public static void parse(List<Integer> listArgs) throws Exception {
+        try (Scanner scanner = new Scanner(new File(argsName.get("path")))) {
+            column = scanner.next().split(argsName.get("delimiter"));
+            String[] args = argsName.get("filter").split(",");
+            for (String arg : args) {
+                for (int index = 0; index < column.length; index++) {
+                    if (arg.equals(column[index])) {
+                        listArgs.add(index);
+                        break;
                     }
                 }
             }
